e80600554d66509421b199476c20f6ce722cdd64
tgmrks <tgmrks@gmail.com>
2016-06-18 16:20:38 -0300

    selectOneRadio example

---
+++ SelectOneRadioComponent/src/main/java/FormBean.java
@@ -0,0 +1,33 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@ViewScoped
+public class FormBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String gender;
+	
+	public void register() {
+		System.out.println("Gender: " + this.gender);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Done!"));
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+	
+	
+}

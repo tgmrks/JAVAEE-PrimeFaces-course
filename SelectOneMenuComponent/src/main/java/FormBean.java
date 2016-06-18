b3d8de1cfc1ff5d8f40ff8534bf9c146b285cfd2
tgmrks <tgmrks@gmail.com>
2016-06-18 14:28:47 -0300

    SelectOne with dependency

--- SelectOneMenuComponent/src/main/java/FormBean.java
+++ SelectOneMenuComponent/src/main/java/FormBean.java
@@ -1,4 +1,6 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -10,13 +12,58 @@ import javax.faces.context.FacesContext;
 public class FormBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	
+	public static final String ASIA = "Asia";
+	public static final String AMERICA = "America";
+	public static final String EUROPE = "Europe";
 
 	private String profession;
 	
+	private String land;
+	private List<String> lands = new ArrayList<>();
+	private String country;
+	private List<String> countries = new ArrayList<>();
+	
+	public FormBean(){
+		lands.add(ASIA);
+		lands.add(EUROPE);
+		lands.add(AMERICA);
+	}
+	
 	public void register() {
 		System.out.println("Profession: " + this.profession);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Done! You Selected: " + this.profession));
+	}
+	
+	public void register2() {
+		System.out.println("Continent: " + this.land);
+		System.out.println("Country: " + this.country);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Done! You selected: " + this.country));
+	}
+	
+	public void loadCountry(){
+		countries.clear();
 		
-		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Done!"));
+		if(ASIA.equals(land)){
+			countries.add("China");
+			countries.add("Japan");
+			countries.add("South Corea");
+		}
+		else if(AMERICA.equals(land)){
+			countries.add("United States of America");
+			countries.add("Canada");
+			countries.add("Mexico");
+			countries.add("Brazil");
+			countries.add("Argentina");
+		}
+		else if(EUROPE.equals(land)){
+			countries.add("France");
+			countries.add("German");
+			countries.add("England");
+			countries.add("Italy");
+			countries.add("Portugal");
+			countries.add("Spain");
+		}
 	}
 	
 	public String getProfession() {
@@ -27,4 +74,36 @@ public class FormBean implements Serializable {
 		this.profession = profession;
 	}
 
+	public String getLand() {
+		return land;
+	}
+
+	public void setLand(String land) {
+		this.land = land;
+	}
+
+	public List<String> getLands() {
+		return lands;
+	}
+
+	public void setLands(List<String> lands) {
+		this.lands = lands;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public List<String> getCountries() {
+		return countries;
+	}
+
+	public void setCountries(List<String> countries) {
+		this.countries = countries;
+	}
+	
 }

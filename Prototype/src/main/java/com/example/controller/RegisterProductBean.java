ae9559f6886e26988d86a6345af3f4f46482fe8b
tgmrks <tgmrks@gmail.com>
2016-07-16 17:31:00 -0300

    configuring postRender and entityManagerProducer

--- Prototype/src/main/java/com/example/controller/RegisterProductBean.java
+++ Prototype/src/main/java/com/example/controller/RegisterProductBean.java
@@ -1,11 +1,18 @@
 package com.example.controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
+import com.example.model.Category;
 import com.example.model.Product;
 
 @Named
@@ -14,7 +21,11 @@ public class RegisterProductBean implements Serializable {
 	
 	private static final long serialVersionUID = 1L; 
 	
+	@Inject //will call a producer that you may configure (refer to: 'com.example.util.jpa.EntityManagerProducer')
+	private EntityManager manager;
 	private Product product;
+	
+	private List<Category> categoriesRoot = new ArrayList<>();
 	
 	public RegisterProductBean(){
 		product = new Product(); 
@@ -22,6 +33,18 @@ public class RegisterProductBean implements Serializable {
 	
 	public void save(){
 		//throw new RuntimeException("testing");
+	}
+	
+	public void init(){								
+		System.out.println("begin transac...");
+
+		/*Good Practise
+		//This is not a good practise to create 'EntityManagerFactory' everytime the method is called. It will demand unnecessary proccess. 
+		//Create a class with 'ApplicationScoped' to handle only one time     //Persistence-unit name
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("OrderOU");
+		*/
+							//That's not the name of your table, but your class	
+		categoriesRoot = manager.createQuery("from Category", Category.class).getResultList();
 	}
 
 	public Product getProduct() {
@@ -31,6 +54,9 @@ public class RegisterProductBean implements Serializable {
 	public void setProduct(Product product) {
 		this.product = product;
 	}
-	
+
+	public List<Category> getCategoriesRoot() {
+		return categoriesRoot;
+	}
 	
 }
